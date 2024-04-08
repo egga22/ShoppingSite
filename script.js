b912295ebd9fb1b3712d12f01ba84e602c83de65
egga22 <84204028+egga22@users.noreply.github.com>
2024-04-08 09:48:51 -0500

    foot

--- script.js
+++ script.js
@@ -352,16 +352,13 @@ function login() {
             // User found
             alert('Login successful');
             localStorage.setItem('isLoggedIn', 'true');
-<<<<<<< HEAD
             // Update login status and fetch cart data immediately after login
             renderCartItems()
-
             localStorage.setItem('username', username); // Store the username in localStorage
             updateLoginStatus();
->>>>>>> c24586c2d0a38c6f9131eb16b08369c217b70d1d
         } else {
             // User not found
-            alert('Login failed');
+            alert('Incorrect username or password!');
         }
     })
     .catch(error => {
